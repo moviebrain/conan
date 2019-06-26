7a022ffdab0a02ef997df605facd1fdd63576984
moviebrain <moviebrain@gmail.com>
2019-06-26 15:48:14 -0700

    added rough scp progress

--- conan.py
+++ conan.py
@@ -2,14 +2,20 @@ import paramiko
 from paramiko import SSHClient
 from scp import SCPClient
 import subprocess
+import sys
 
+def progress(filename, size, sent):
+    sys.stdout.write("%s\'s progress: %.2f%%   \r" % (filename, float(sent)/float(size)*100) )
+
+# SCPCLient takes a paramiko transport and progress callback as its arguments.
+# scp = SCPClient(ssh.get_transport(), progress = progress)
 ssh= SSHClient()
 ssh.load_system_host_keys()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 print('connecting...')
 ssh.connect(hostname='192.168.7.125',username='steve',password='steve')
 print('connected.') # SCPClient takes a paramiko transport as an argument
-scp = SCPClient(ssh.get_transport())
+scp = SCPClient(ssh.get_transport(), progress = progress)
 print('getting file')
 scp.get('/home/steve/steam/exiles/ConanSandbox/Saved/Logs/ConanSandbox.log')
 print('file retrieved?')
